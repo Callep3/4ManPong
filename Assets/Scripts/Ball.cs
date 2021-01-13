587aeec3a8ce83910396b63b9403d9448d32c6e2
Callep3 <42645914+Callep3@users.noreply.github.com>
2021-01-13 17:12:00 +0100

    Everything
    Working Ball
    Working Player
    Working NetworkManager
    
    Addition of assets

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using Mirror.Examples.Benchmark;
+using Random = UnityEngine.Random;
+
+public class Ball : NetworkBehaviour
+{
+    private Vector3 direction;
+    [SerializeField] private float ballSpeed;
+
+    private void Start()
+    {
+        //TODO fix so that Y is randomized when you have more than 2 players
+        int x = 0;
+        int y = 0;
+        while (x == 0)
+        {
+            x = (int)Random.Range(-1f, 2f);
+        }
+        direction = new Vector3(x, y, 0);
+        ballSpeed = 4f;
+    }
+
+    private void Update()
+    {
+        BallMovement();
+    }
+
+    private void BallMovement()
+    {
+        transform.position += direction.normalized * (ballSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        BallBounce(other);
+    }
+
+    private void BallBounce(Collider2D other)
+    {
+        Debug.Log("Bounce");
+        if (other.CompareTag("Player"))
+        {
+            direction = (transform.position - other.transform.position).normalized;
+        }
+        else
+        {
+            if (other.CompareTag("HorizontalWall")) direction.y *= -1;
+            if (other.CompareTag("VerticalWall")) direction.x *= -1;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    private Vector3 movement = new Vector3();
+    [SerializeField] private float speed;
+
+    private void Start()
+    {
+        speed = 2f;
+    }
+
+    [Client]
+    private void Update()
+    {
+        if (!hasAuthority) return;
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)
+        || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            if (transform.rotation.z == 0)
+            {
+                CmdMove("Vertical");
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)
+        || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            if (transform.rotation.z == -90)
+            {
+                CmdMove("Horizontal");
+            }
+        }
+    }
+    
+    private void CmdMove(String movementType)
+    {
+        if (movementType == "Vertical")
+        {
+            transform.position += new Vector3(0, Input.GetAxisRaw(movementType), 0) * (speed * Time.deltaTime);
+        }
+        if (movementType == "Horizontal")
+        {
+            transform.position += new Vector3(Input.GetAxisRaw(movementType), 0, 0) * (speed * Time.deltaTime);
+        }
+    }
+}
